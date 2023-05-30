26692dd214373176daa0d59061fef312b6c5f2fa
Marcelo <marcelo.sobrinho@outlook.com>
2023-05-30 16:48:08 -0300

    fix: product page

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -18,48 +18,52 @@ import React from 'react';
 
 const { Text, Title } = Typography;
 
-function Product({ product }) {
+function Product({ loading, product }) {
   return (
-    <Space align="center" split={<Divider type="vertical" />}>
-      <Image
-        width={400}
-        height={460}
-        src={product?.image}
-      />
-      <Space direction="vertical" style={{ width: 600 }}>
-        <Space align="start">
-          <Rating
-            allowFraction
-            readonly
-            initialValue={product?.rating.rate}
-            size={20}
+    <div>
+      {loading ? null : (
+        <Space align="center" size={[100]} style={{ marginTop: 50, marginBottom: 50, justifyContent: 'center', width: '100%' }}>
+          <Image
+            width={400}
+            height={460}
+            src={product?.image}
           />
-          <Text strong style={{ fontSize: 14 }}>{`(${product?.rating.count})`}</Text>
-        </Space>
-        <Space direction="vertical">
-          <Title>{product?.title}</Title>
-          <Text>{product?.description}</Text>
-          <Text>Tamanho:</Text>
-          <Radio.Group defaultValue="p" buttonStyle="solid">
-            <Radio.Button value="a">P</Radio.Button>
-            <Radio.Button value="b">M</Radio.Button>
-            <Radio.Button value="c">G</Radio.Button>
-          </Radio.Group>
-          <Text strong style={{ fontSize: 24 }}>
-            {`R$ ${product?.price.toLocaleString('pt-BR', { minimumFractionDigits: 2 })}`}
-          </Text>
-          <Space>
-            <Button shape="circle" size="small">-</Button>
-            <Input defaultValue={1} maxLength={2} type="number" style={{ width: 80 }} />
-            <Button shape="circle" size="small">+</Button>
+          <Space direction="vertical" style={{ width: 600 }}>
+            <Space direction="vertical">
+              <Title style={{ marginBottom: 12 }}>{product?.title}</Title>
+              <Space align="start" style={{ marginBottom: 10 }}>
+                <Rating
+                  allowFraction
+                  readonly
+                  initialValue={product?.rating.rate}
+                  size={20}
+                />
+                <Text strong style={{ fontSize: 14 }}>{`(${product?.rating.count})`}</Text>
+              </Space>
+              <Text>{product?.description}</Text>
+              <Text strong>Tamanho:</Text>
+              <Radio.Group defaultValue="p" buttonStyle="solid">
+                <Radio.Button value="a">P</Radio.Button>
+                <Radio.Button value="b">M</Radio.Button>
+                <Radio.Button value="c">G</Radio.Button>
+              </Radio.Group>
+              <Text strong style={{ fontSize: 24 }}>
+                {`R$ ${product?.price.toLocaleString('pt-BR', { minimumFractionDigits: 2 })}`}
+              </Text>
+              <Space style={{ marginTop: 6 }}>
+                <Button shape="circle" size="small">-</Button>
+                <Input defaultValue={1} maxLength={2} type="number" style={{ width: 80 }} />
+                <Button shape="circle" size="small">+</Button>
+              </Space>
+              <Space style={{ marginTop: 12 }}>
+                <Button size="large" type="primary">Comprar agora</Button>
+                <Button icon={<ShoppingCartOutlined />} size="large">Adicionar ao carrinho</Button>
+              </Space>
+            </Space>
           </Space>
-          <Space>
-            <Button size="large" type="primary">Comprar agora</Button>
-            <Button icon={<ShoppingCartOutlined />} size="large">Adicionar ao carrinho</Button>
-          </Space>
         </Space>
-      </Space>
-    </Space>
+      )}
+    </div>
   );
 }
 
